cbc26ca18df205340ad6e4a14a29aaaa6a47ef0e
chain-chainger <chain.chainger@gmail.com>
2024-01-16 15:50:50 +0900

    feat: 홈페이지에 로그인 버튼 추가 및 클릭시 42 로그인 페이지로 이동 구현

--- frontend/public/js/pages/HomePage.js
+++ frontend/public/js/pages/HomePage.js
@@ -4,16 +4,27 @@ export default class Home extends Component {
     setup() {
         this.$state = this.$props;
     }
+    
     template() {
+      let loginOrGameContent = "";
+      switch (this.$state.loginState) {
+        case true:
+          loginOrGameContent = `
+          <div class="btn-box">
+          <a class="btn btn-primary" href="#/game" role="button">게임플레이</a>
+          </div>
+          `
+        case false:
+          loginOrGameContent = `
+          <button id="login-button">Login</button>
+          `
+      }
         return `
-        <button id="loginbtn">로그인</button>
-    <div class="main-box">
-      <div class="btn-box">
-        <a class="btn btn-primary" href="#/game" role="button">게임플레이</a>
-      </div>
-      <img class="pikachu-image" src="../images/background.jpeg"></img>
-    </div>
-    `;
+        <div class="main-box">
+        ${loginOrGameContent}
+        <img class="pikachu-image" src="../images/background.jpeg"></img>
+        </div>
+        `;
     }
 
     setEvent() {
@@ -24,4 +35,14 @@ export default class Home extends Component {
     login() {
         this.$state.login();
     }
+    moveFortyTwoOAuth() {
+      const authorizationUrl = `https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-b677e803809d207e81ae3a321bdf542af8d318ca330d81824e4b972bca224918&redirect_uri=http%3A%2F%2F127.0.0.1&response_type=code`;
+      window.location.href = authorizationUrl;
+    }
+
+  setEvent() {
+    this.addEvent('click', '#login-Button', ({ target }) => {
+      this.moveFortyTwoOAuth();
+    });
+  }
 }
