311180d57561806807bc6bef07b3d467fc857f4e
chain-chainger <chain.chainger@gmail.com>
2024-01-16 21:40:08 +0900

    feat: 로그인 페이지 및 OTP 컴포넌트 생성

---
+++ frontend/public/js/components/Otp.js
@@ -0,0 +1,81 @@
+import Component from '../core/Component.js';
+
+export default class Otp extends Component {
+  setup() {
+    this.$state = { otpString: ['', '', '', '', '', ''] };
+  }
+
+  template() {
+    return `
+    <div class="otp-card">
+      <h1>OTP Verification Form</h1>
+      <p>Code has been sent to your email</p>
+      <div class="otp-card-inputs">
+        <input type="text" maxlength="1" autofocus>
+        <input type="text" maxlength="1">
+        <input type="text" maxlength="1">
+        <input type="text" maxlength="1">
+        <input type="text" maxlength="1">
+        <input type="text" maxlength="1">
+      </div>
+      <button id="verifyButton" ${this.isVerifyButtonDisabled() ? 'disabled' : ''}>Verify</button>
+    </div>
+    `;
+  }
+
+  setEvent() {
+    this.addEvent('input', '.otp-card-inputs input', (e) => this.handleInput(e));
+    this.addEvent('keydown', '.otp-card-inputs input', (e) => this.handleKeyDown(e));
+    this.addEvent('click', '#verifyButton', () => this.handleButtonClick());
+    this.addEvent('click', '#resendLink', (e) => this.handleResendClick(e));
+  }
+
+  handleInput(e) {
+    const currentElement = e.target;
+    const nextElement = currentElement.nextElementSibling;
+
+    const reg = /^[0-9]+$/;
+    if (reg.test(currentElement.value) === false) {
+      currentElement.value = '';
+    } else {
+      this.$state.otpString[Array.from(currentElement.parentNode.children).indexOf(currentElement)] = currentElement.value;
+      if (currentElement.value && nextElement) {
+        nextElement.focus();
+      }
+    }
+
+    this.updateVerifyButtonState();
+  }
+
+  handleKeyDown(e) {
+    const currentElement = e.target;
+    const prevElement = currentElement.previousElementSibling;
+    const currentIndex = Array.from(currentElement.parentNode.children).indexOf(currentElement);
+    const prevIndex = prevElement ? Array.from(prevElement.parentNode.children).indexOf(prevElement) : 0;
+
+    if (e.keyCode === 8) {
+      if (currentIndex == 5) {
+        this.$state.otpString[currentIndex] = ''; 
+      }
+      if (currentElement.value === '' && prevElement) {
+        this.$state.otpString[prevIndex] = '';
+        prevElement.focus();
+      }
+    }
+  }
+
+  handleButtonClick() {
+    console.log(this.$state.otpString.join(''));
+  }
+
+  updateVerifyButtonState() {
+    const lastInput = this.$state.otpString[this.$state.otpString.length - 1];
+    const isLastInputFilled = lastInput.trim() !== '';
+    console.log(this.$state.otpString);
+    this.$target.querySelector('#verifyButton').disabled = !isLastInputFilled;
+  }
+
+  isVerifyButtonDisabled() {
+    return this.$state.otpString.some(value => value.trim() === '');
+  }
+}
--- frontend/public/js/pages/LoginPage.js
+++ frontend/public/js/pages/LoginPage.js
@@ -1,9 +1,19 @@
 import Component from '../core/Component.js';
+import Otp from '../components/Otp.js';
+
 
 export default class Login extends Component {
   template() {
     return `
     <h2>Login Page</h2>
+    <div data-component='Otp'></div>
     `;
   }
+
+  mounted() {
+    const $otp = this.$target.querySelector(
+      "[data-component='Otp']"
+    );
+    new Otp($otp);
+    }
 }
