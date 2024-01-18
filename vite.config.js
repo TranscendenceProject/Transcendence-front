20998e893b84ca5710b370e4d9fa3beb4cafc7bc
Seongsu Park <56729421+seongs1024@users.noreply.github.com>
2024-01-18 19:35:13 +0900

    fix: CSS 파일 찾을 수 없음 해결

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ export default {
   server: {
     port: 3000,
     hot: true
+  },
+  resolve: {
+    alias: {
+      '@bootstrap': 'node_modules/bootstrap',
+      '@jquery': 'node_modules/jquery'
+    }
   }
 }
