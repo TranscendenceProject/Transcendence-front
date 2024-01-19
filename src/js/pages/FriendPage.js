9128543ab14308b59bc09a5bda12fb6d7772b37d
chain-chainger <chain.chainger@gmail.com>
2024-01-19 16:19:24 +0900

    feat: 친구 페이지에 유저 검색, 친구 관리 메뉴 버튼 생성

--- src/js/pages/FriendPage.js
+++ src/js/pages/FriendPage.js
@@ -1,9 +1,37 @@
 import Component from '../core/Component.js';
 
 export default class Login extends Component {
+
+  setup() {
+    this.$state = {
+      currentMenu: 'default',
+    };
+  }
+
   template() {
     return `
-    <h2>Friend Page</h2>
+    <div class="main-box">
+      <div class="btn-box">
+        <ul>
+          <li>
+            <button class="btn btn-primary" id="searchUser" role="button">유저 검색</button>
+          </li>
+          <li>
+            <button class="btn btn-primary" id="manageFriend" role="button">친구 관리</button>
+          </li>
+        </ul>
+      </div>
+      <img class="pikachu-image" src="../assets/background.jpeg"></img>
+      </div>
     `;
+  }
+
+  setEvent() {
+    this.addEvent("click", "#searchUser", ({ target }) => {
+      this.setState({ currentMenu: "searchUser" });
+    })
+    this.addEvent("click", "#manageFriend", ({ target }) => {
+      this.setState({ currentMenu: "manageFriend" });
+    })
   }
 }
