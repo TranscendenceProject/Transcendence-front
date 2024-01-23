a2d471a6d756c6134dc1d6f58240c62bde307009
chain-chainger <chain.chainger@gmail.com>
2024-01-23 13:46:50 +0900

    feat: 친구관리 컴포넌트 친구리스트 API 요청 및 응답에 따른 렌더링 구현

--- src/js/components/ManageFriend.js
+++ src/js/components/ManageFriend.js
@@ -1,20 +1,24 @@
 import Component from "../core/Component.js";
+import api from "../api/http.js";
 
 export default class ManageFriend extends Component {
+  constructor($target, $props) {
+    this.super();
+    // this.getFriendsList();
+  } 
+
   setup() {
     this.$state = {
       firends: [
         {
-          intraId: 'seunghwk',
-          nickname: 'seunghwk',
-          isLogined: true,
-          profilePictureUrl: 'assets/dummyProfile.jpg'
+          user_id: "1",
+          intra_id: "seunghwk",
+          login_status: true
         },
         {
-          intraId: 'gsong',
-          nickname: 'gsong',
-          isLogined: false,
-          profilePictureUrl: 'assets/dummyProfile.jpg'
+          user_id: '2',
+          intra_id: 'gsong',
+          login_status: false,
         },
       ],
     };
@@ -26,14 +30,28 @@ export default class ManageFriend extends Component {
       <h2>친구 관리</h2>
       <div> ${this.$state.firends.map(user => `
         <div>
-          <img src="${user.profilePictureUrl}" alt="Profile Picture">
-          <p>Intra ID: ${user.intraId}</p>
-          <p>Nickname: ${user.nickname}</p>
-          <p>IsLogined: ${user.isLogined ? '로그인' : '로그아웃'}</p>
+          <p>User ID: ${user.user_id}</p>
+          <p>Intra ID: ${user.intra_id}</p>
+          <p>IsLogined: ${user.login_status ? '로그인' : '로그아웃'}</p>
           <button>친구 삭제</button>
         </div>
       `).join('')}</div>
     </div>
     `;
+  }
+
+  async getFriendsList() {
+    const url = 'http://127.0.0.1:8000/firends/list';
+    const token = localStorage.getItem('token');
+    const headers = { 'token': token };
+    try {
+      const response = await api.get(url, headers);
+
+      this.setState({
+        firends: response.firends,
+      });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   }
 }
