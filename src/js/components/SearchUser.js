c48e4e5603ea829e33f5c731ebdc17e2a057743a
chain-chainger <chain.chainger@gmail.com>
2024-01-23 19:17:01 +0900

    feat: 유저 검색 컴포넌트 검색 버튼 클릭 이벤트 발생시 API 요청 및 응답에 따른 렌더링 구현

--- src/js/components/SearchUser.js
+++ src/js/components/SearchUser.js
@@ -5,16 +5,16 @@ export default class SearchUser extends Component {
     this.$state = {
       users: [
         {
-          intraId: 'seunghwk',
-          nickname: 'seunghwk',
-          isFriend: true,
-          profilePictureUrl: 'assets/dummyProfile.jpg'
+          user_id: '1',
+          intra_id: 'seunghwk',
+          login_status: 'true',
+          is_friend: 'false'
         },
         {
-          intraId: 'gsong',
-          nickname: 'gsong',
-          isFriend: false,
-          profilePictureUrl: 'assets/dummyProfile.jpg'
+          user_id: '1',
+          intra_id: 'gsong',
+          login_status: 'false',
+          is_friend: 'true'
         },
       ],
     };
@@ -25,17 +25,70 @@ export default class SearchUser extends Component {
     <div>
       <h2>유저 검색</h2>
       <p>유저명</p>
-      <input type="text" value=""></input>
-      <button>검색</button>
+      <input type="text" class="searchInput" value="">
+      <button class="searchButton">검색</button>
       <div> ${this.$state.users.map(user => `
-      <div>
-        <img src="${user.profilePictureUrl}" alt="Profile Picture">
-        <p>Intra ID: ${user.intraId}</p>
-        <p>Nickname: ${user.nickname}</p>
-        <p>Is Friend: ${user.isFriend ? '친구' : '<button>친구 추가</button>'}</p>
+        <div>
+          <p>Intra ID: ${user.intra_id}</p>
+          <p>로그인 상태: ${user.login_status === "true" ? '로그인 중' : '로그아웃'}</p>
+          ${user.is_friend === "true" ?
+          '<p>친구</p>' : `<button class="addButton" data-user-id="${user.user_id}">친구 추가</button>`}
         </div>
       `).join('')}</div>
     </div>
     `;
+  }
+
+  setEvent() {
+    this.addEvent('click', '.searchButton', (e) => this.handleSearchButtonClick(e));
+  }
+
+  handleSearchButtonClick() {
+    const searchInput = this.$target.querySelector('.searchInput');
+    const searchValue = searchInput.value;
+
+    console.log('검색어:', searchValue);
+    // this.searchUser(searchValue);
+
+
+    // // 테스트 코드
+    // this.setState({
+    //   users: [
+    //     {
+    //       user_id: '1',
+    //       intra_id: 'test',
+    //       login_status: 'true',
+    //       is_friend: 'false'
+    //     },
+    //     {
+    //       user_id: '1',
+    //       intra_id: 'test',
+    //       login_status: 'false',
+    //       is_friend: 'true'
+    //     },
+    //     {
+    //       user_id: '1',
+    //       intra_id: 'test',
+    //       login_status: 'false',
+    //       is_friend: 'true'
+    //     },
+    //   ],
+    // });
+  }
+
+  async searchUser(searchValue) {
+    const url = `http://127.0.0.1:8000/friends/search?query=${searchInput}`;
+    const token = localStorage.getItem('token');
+    const headers = { 'token': token };
+
+    try {
+      const response = await api.get(url, headers);
+
+      this.setState({
+        users: response.users,
+      });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   }
 }
