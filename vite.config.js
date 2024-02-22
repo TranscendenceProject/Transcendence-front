365198439ce46a841c589de4f3ef4675de78bfd7
Seongsu Park <56729421+seongs1024@users.noreply.github.com>
2024-02-22 23:42:44 +0900

    update: TLS 주입으로 해결(vite plugin이 인증서 생성하지 않도록 변경)

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite"
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import fs from 'fs';
 
 export default defineConfig ({
   root: './src',
@@ -7,15 +7,16 @@ export default defineConfig ({
   envPrefix: 'TS_',
   server: {
     port: 443,
-    hot: true
+    hot: true,
+    https: {
+      key: fs.readFileSync('./tls/cert.key.pem'),
+      cert: fs.readFileSync('./tls/cert.pem')
+    }
   },
   resolve: {
     alias: {
       '@bootstrap': 'node_modules/bootstrap',
       '@jquery': 'node_modules/jquery'
     }
-  },
-  plugins: [
-    basicSsl()
-  ]
+  }
 })
